d8846563295e40ded28829b606225d7c045aad67
Rakshit Malik <rakshitmalik156@gmail.com>
2023-03-31 17:20:24 +0530

    dope man

---
+++ banana.js
@@ -0,0 +1,28 @@
+stats = "";
+img = "";
+objectDetector = "";
+function preload(){
+    img = loadImage("photo4(bananas).jpg");
+}
+function setup(){
+    canvas = createCanvas(900,500);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+function draw(){
+    image(img,200,0,500,500);
+}
+function modelloaded(){
+    console.log("cocossd loaded");
+    objectDetector.detect(img,gotResults);
+    stats = true;
+}
+function gotResults(error,result){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(result)
+    }
+}
